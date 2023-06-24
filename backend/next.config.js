17505c03a4288d9d34cd4feae96bd4493fd94150
Narongrit Somjaiuraikul <pre.naronglit@gmail.com>
2023-06-24 17:06:08 +0700

    Add CORS

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var whitelist = ['https://khongchai.github.io/SonThaiMaiDai']
+var corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
