d5587694105abfffd72dba838324f62c96eb644a
Narongrit Somjaiuraikul <pre.naronglit@gmail.com>
2023-06-24 19:51:38 +0700

    Remove reactStrictMode

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-}
+
 
 var express = require('express')
 var cors = require('cors')
 var app = express()
-var whitelist = ['https://khongchai.github.io/SonThaiMaiDai']
 
 // Define CORS options
 const corsOptions = {
-    origin: 'https://example.com', // Only allow requests from example.com
+    origin: 'https://khongchai.github.io/SonThaiMaiDai', // Only allow requests
 };
 app.get('*', cors(corsOptions), function(req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for only example.com.' })
-})
+    res.json({ msg: 'This is CORS-enabled for only khongchai.github.io/SonThaiMaiDai' })
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
