cc82f669052ea05e75d89df995a55f2bffd332ea
khongchai <k.greesuradej@accenture.com>
2023-06-25 02:10:44 +0700

    ok screw this

--- backend/src/pages/api/cors.ts
+++ backend/src/pages/api/cors.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 export default function customCors(req: NextApiRequest, res: NextApiResponse) {
-  if (process.env.NODE_ENV === "production") {
-    console.log("about to call custome cors");
-    return req.headers.host?.includes(
-      "khongchai.github.io",
-    );
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   console.log("about to call custome cors");
+  //   return req.headers.host?.includes(
+  //     "khongchai.github.io",
+  //   );
+  // }
 
   return true;
 }
