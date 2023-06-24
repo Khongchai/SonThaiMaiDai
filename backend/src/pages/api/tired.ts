1aff788ee264abef2ca0eed74cb2bd7dcb3f4a2d
khongchai <k.greesuradej@accenture.com>
2023-06-25 03:06:47 +0700

    implemented tired endpoint

--- backend/src/pages/api/tired.ts
+++ backend/src/pages/api/tired.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 type Data = {
-  sentence: string;
+  result: string;
 };
 
 const sentences = [
@@ -10,15 +11,24 @@ const sentences = [
   "กูเหนื่อยมากแล้วไอเหี้ย หยุดถามซักที",
   "ถามอยู่นั่นแหละ!",
   "โอ้ยเมื่อไรจะหยุดถาม!",
+  "ไม่รู้โว้ยยยยย",
+  "เบื่อ เบื่อ เบื่อ คนนี้ถามเยอะ AI ก็ขี้เกียจเป็นนะ",
+  "ถามเราเยอะชอบเราป่าว",
 ];
 function pickSentence() {
   const index = Math.floor(Math.random() * sentences.length);
   return sentences[index];
 }
 
-export default function handler(
-  _: NextApiRequest,
+export default async function handler(
+  req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ sentence: pickSentence() });
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  res.status(200).json({ result: pickSentence() });
 }
