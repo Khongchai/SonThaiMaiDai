63afe2e1ca985425f0cd6fb71599b13e73a45382
Narongrit Somjaiuraikul <pre.naronglit@gmail.com>
2023-06-24 20:27:59 +0700

    Update config.js to handle CORS

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-// Define CORS options
-const corsOptions = {
-    origin: 'https://khongchai.github.io/SonThaiMaiDai', // Only allow requests
+module.exports = {
+    async headers() {
+        return [{
+            source: '/(.*)', // Match all pages
+            headers: [{
+                key: 'Access-Control-Allow-Origin',
+                value: 'https://khongchai.github.io/SonThaiMaiDai', // Allow requests from this origin
+            }, ],
+        }, ];
+    },
 };
-app.use(cors(corsOptions));
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+
+// var express = require('express')
+// var cors = require('cors')
+// var app = express()
+
+// // Define CORS options
+// const corsOptions = {
+//     origin: 'https://khongchai.github.io/SonThaiMaiDai', // Only allow requests
+// };
+// app.use(cors(corsOptions));
+// app.listen(3000, () => {
+//     console.log('Server started on port 3000');
+// });
