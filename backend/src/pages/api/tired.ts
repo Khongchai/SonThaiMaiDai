a27a5874aacbc104c9053838c2cdce8a5d6c2f92
khongchai <k.greesuradej@accenture.com>
2023-06-24 20:47:26 +0700

    added more stuff to tired

--- backend/src/pages/api/tired.ts
+++ backend/src/pages/api/tired.ts
@@ -1,14 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  sentence: string
+  sentence: string;
+};
+
+const sentences = [
+  "เหนื่อยแล้วไม่ตอบละะ",
+  "กูเหนื่อยมากแล้วไอเหี้ย หยุดถามซักที",
+  "ถามอยู่นั่นแหละ!",
+  "โอ้ยเมื่อไรจะหยุดถาม!",
+];
+function pickSentence() {
+  const index = Math.floor(Math.random() * sentences.length);
+  return sentences[index];
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-    res.status(200).json({ sentence :  'กูเหนื่อยมากแล้วไอเหี้ย หยุดถามซักที' })
+  res.status(200).json({ sentence: pickSentence() });
 }
